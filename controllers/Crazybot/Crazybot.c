1ba862eef25e9de33c8770d2de050c6f97460f68
Angelo Espinoza <al1609046@upy.edu.mx>
2019-07-02 13:54:40 -0500

    Distance sensor added and configured

---
+++ controllers/Crazybot/Crazybot.c
@@ -0,0 +1,106 @@
+/*
+ * File:          Crazybot.c
+ * Date:
+ * Description:
+ * Author:
+ * Modifications:
+ */
+
+/*
+ * You may need to add include files like <webots/distance_sensor.h> or
+ * <webots/differential_wheels.h>, etc.
+ */
+#include <webots/robot.h>
+#include <webots/motor.h> 
+#include <stdio.h>
+#include <webots/keyboard.h>
+/*
+ * You may want to add macros here.
+ */
+#define TIME_STEP 64
+
+/*
+ * This is the main program.
+ * The arguments of the main function can be specified by the
+ * "controllerArgs" field of the Robot node
+ */
+int main(int argc, char **argv)
+{
+  /* necessary to initialize webots stuff */
+  wb_robot_init();
+  /*
+   * You should declare here WbDeviceTag variables for storing
+   * robot devices like this:
+   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
+   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
+   */
+  WbDeviceTag motor_right = wb_robot_get_device("motor_right");
+  WbDeviceTag motor_left = wb_robot_get_device("motor_left");
+  wb_keyboard_enable(TIME_STEP);
+  //int key;
+
+  /* main loop
+   * Perform simulation steps of TIME_STEP milliseconds
+   * and leave the loop when the simulation is over
+   */
+  wb_motor_set_position(motor_left, INFINITY);
+  wb_motor_set_position(motor_right, INFINITY);  
+  while (wb_robot_step(TIME_STEP) != -1) 
+  {
+     double speed = 1.5; //[rad/s] 
+     
+     /* RIGHT */
+     
+     wb_motor_set_velocity(motor_right, speed);
+          
+     /* LEFT */
+
+     wb_motor_set_velocity(motor_left, speed);
+     
+     printf("HelloWorld");
+     
+    /*
+     * Read the sensors :
+     * Enter here functions to read sensor data, like:
+     *  double val = wb_distance_sensor_get_value(my_sensor);
+     */
+/*     enum 
+     {
+      WB_KEYBOARD_END,
+      WB_KEYBOARD_HOME,
+      WB_KEYBOARD_LEFT,
+      WB_KEYBOARD_UP,
+      WB_KEYBOARD_RIGHT,
+      WB_KEYBOARD_DOWN,
+      WB_KEYBOARD_PAGEUP,
+      WB_KEYBOARD_PAGEDOWN,
+      WB_KEYBOARD_NUMPAD_HOME,
+      WB_KEYBOARD_NUMPAD_LEFT,
+      WB_KEYBOARD_NUMPAD_UP,
+      WB_KEYBOARD_NUMPAD_RIGHT,
+      WB_KEYBOARD_NUMPAD_DOWN,
+      WB_KEYBOARD_NUMPAD_END,
+      WB_KEYBOARD_KEY,
+      WB_KEYBOARD_SHIFT,
+      WB_KEYBOARD_CONTROL,
+      WB_KEYBOARD_ALT
+      };
+*/
+    //key = wb_keyboard_get_key();
+      
+      
+    /* Process sensor data here */
+
+    /*
+     * Enter here functions to send actuator commands, like:
+     * wb_differential_wheels_set_speed(100.0,100.0);
+     */
+  };
+
+  /* Enter your cleanup code here */
+
+  /* This is necessary to cleanup webots resources */
+  wb_robot_cleanup();
+
+  return 0;
+}
